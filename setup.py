03517228ec9a7c24beb35940410b6bc761b3b907
Eniatorudabo Abomaye <abomayesan@gmail.com>
2023-04-14 05:36:55 +0100

    added setup.py and requirements.txt

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import find_packages, setup
+from typing import List
+
+
+def get_requirements(path:str)->List[str]:
+    """
+    Function to return a list of required packages for this project
+    """
+    requirements = []
+
+    with open(path, "r") as f:
+        requirements = f.readlines()
+        requirements = [req.replace("\m", "") for req in requirements]
+
+        if "-e ." in requirements:
+            requirements.remove("-e .")
+
+    return requirements
+
+
+setup(
+    name = "mlops project",
+    version = "0.0.2",
+    author = "Abomaye Victor",
+    author_email = "abomayesan@gmail.com",
+    packages = find_packages(),
+    install_requires = get_requirements("requirements.txt")
+)
